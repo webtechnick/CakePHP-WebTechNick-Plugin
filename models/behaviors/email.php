eece42112a9bc19e9edf794b267ec1c4abf542bb
webtechnick <nick@webtechnick.com>
2010-01-30 22:46:19 +0000

    Finished 0.2 release of email behavior for WTN goodies.

--- models/behaviors/email.php
+++ models/behaviors/email.php
@@ -4,18 +4,31 @@
 * This is useful for sending quick emails based off a model and for testing
 * purposes.
 *
-* @author Nick Baker nick [at] webtechnick.com
+* Usage: 
+* var $actsAs = array('WebTechNick.Email');
+*
+* @author Nick Baker nick[at]webtechnick[dot]com
 * @link http://www.webtechnick.com
-* @version 0.1
+* @version 0.2
 * @license MIT
 */
 App::import('Component', 'Email');
+
+class ExtendedEmail extends EmailComponent{
+  /**
+    * Force Bypass of the template
+    */
+  function send($content = null, $template = null, $layout = null){
+    parent::send($content, null, $layout);
+  }
+}
+
 class EmailBehavior extends ModelBehavior {
 
   function setUp(&$Model, $options = array()){
     $this->options = $options;
     
-    $Model->Email =& new EmailComponent();
+    $Model->Email =& new ExtendedEmail();
   }
 }
 ?>
